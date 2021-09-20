bddd27f49ac63bdc4c1234dcfed5da7aab8a206d
aminkam <aminkammoun001@gmail.com>
2021-09-20 22:40:53 +0100

    Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    clients : [
+      {id : 1, nom : "Amin Kammoun"},
+      {id : 2, nom : "Ahmed Mechim"},
+      {id : 3, nom : "Khaled Boudaya"},
+      {id : 4, nom: "Heni Dammak" }
+    ],
+    nouveauClient : '',
+    isShow :true
+  };
+
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const id = new Date ().getTime();
+    const nom = this.state.nouveauClient;
+    const client = {id : id, nom:nom}
+    const clients = this.state.clients.slice();
+    clients.push(client);
+    this.setState({clients : clients});
+  }
+  
+  delete = amin => {
+    const clients = this.state.clients.slice();
+    const index = clients.findIndex(client => client.id === amin);
+    clients.splice(index, 1) ;
+    this.setState({clients: clients})
+    
+  }
+  change = (event) =>{ 
+    const value =event.currentTarget.value;
+    this.setState({nouveauClient : value})
+  }
+  show = () => {this.setState({isShow: !this.state.isShow})}
+
+
+  render() {
+    return (
+      <div className="box">
+        <button className="hide" onClick={()=> this.show()}>{this.state.isShow? "Hide" : "show"}</button>
+        <div className="part">
+        {
+          this.state.isShow &&  <div className="show">
+            <form onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Ajouter un Nom" value= {this.state.nouveauClient} onChange={this.change}/>
+            <button>Confirmer</button>
+            </form>
+              <ul>
+              {this.state.clients.map(client => (
+                <li>
+                  {client.nom}
+                  <button onClick={()=> this.delete(client.id)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+            
+          </div>
+            
+        }
+        </div>
+        
+        
+        
+      
+      </div>
+    )
+  }
 }
-
-export default App;
